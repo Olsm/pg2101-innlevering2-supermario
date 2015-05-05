55fab2ae0c787345f7ef51bf12f7bbd314d91252
OlavOlsm <OlavOlsm@gmail.com>
2015-05-05 12:46:08 +0200

    scripts lagt til i prosjektet

---
+++ Assets/Scripts/powerCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class powerCube : MonoBehaviour {
+	private Animator animator;
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+
+	}
+
+	void OnCollisionEnter2D (Collision2D col){
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+}
