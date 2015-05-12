0ac5cc7b11c4ed277b16289b4d9dfd6eb6a260d4
OlavOlsm <OlavOlsm@gmail.com>
2015-05-12 19:21:56 +0200

    ubrukt kode kommentert bort

--- Assets/Scripts/gameLogic.cs
+++ Assets/Scripts/gameLogic.cs
@@ -2,14 +2,14 @@
 using System.Collections;
 
 public class gameLogic : MonoBehaviour {
-	private GameObject mario;
-	private GameObject powerCube;
+	//private GameObject mario;
+	//rivate GameObject powerCube;
 	//private GameObject brick;
 
 	// Use this for initialization
 	void Start () {
-		powerCube = GameObject.Find ("SporsmalsCube");
-		mario = GameObject.Find ("SuperMarioSmall");
+		//powerCube = GameObject.Find ("QuestonBlock");
+		//mario = GameObject.Find ("SuperMarioSmall");
 	}
 	
 	// Update is called once per frame
