192f4367897865cbded2468f15f5cf6b7a2314c1
OlavOlsm <OlavOlsm@gmail.com>
2015-05-12 19:30:51 +0200

    Fikset bugs og mer kollisjonsfunksjonalitet lagt til: - Fikset bugs med kollisjoner og bevegelse - mario kan hoppe kort ved kort klikk og langt ved langt klikk - poeng animasjon startes på questionblocks med coin - la til kollisjon for flaggstang

--- Assets/Scripts/platformerController.cs
+++ Assets/Scripts/platformerController.cs
@@ -1,42 +1,47 @@
 using UnityEngine;
 using System.Collections;
 
-	public class platformerController : MonoBehaviour
-	{
+public class platformerController : MonoBehaviour
+{
 	private GameObject mario;
 	private GameObject powerCube;
 	private GameObject brick;
-	private float marioSpeed = 1f; //Definer fart for paddlen
-		private Animator animator;
-		private bool isWalking;
-		private bool movingRight;
-		private bool movingLeft;
+	public float marioSpeed = 0.025f; //Definer fart for paddlen
+	private Animator animator;
+	private bool isWalking;
+	private bool movingRight;
+	private bool movingLeft;
 	private float yPos = 0.32f;
-	private float jumpPower = 4f;
-	private bool touchdown = false;
+	private float touchdownPos = 0.338f;
+	public float jumpPower = 2f;
+	public float jumpHeight = 0.55f;
+	public float maxHeight = 2.3f;
+	public bool touchdown = false;
 	private Rigidbody2D rb;
-	private bool jumping = false;
-
-	private BoxCollider2D bodyCollider;
+	private bool jump = false;
+	public bool jumping = false;
+	public bool fallDown = false;
+	
+	private PolygonCollider2D bodyCollider;
 	public BoxCollider2D headCollider;
 	public BoxCollider2D feetCollider;
-		
-		private Vector2 playerPos =  new Vector2(0.15f, 0.32f); //Utgangsposisjon
-
-		void Start() {
-			rb = GetComponent <Rigidbody2D>();
-			powerCube = GameObject.Find ("QuestionBlock");
-			mario = GameObject.Find ("SuperMarioSmall");
-			bodyCollider = mario.GetComponent <BoxCollider2D>();
-			animator = mario.GetComponent <Animator>();
-		}
-		
-		void startWalk(){
+	
+	private Vector2 playerPos =  new Vector2(0.15f, 0.32f); //Utgangsposisjon
+	
+	void Start() {
+		rb = GetComponent <Rigidbody2D>();
+		powerCube = GameObject.Find ("QuestionBlock");
+		mario = GameObject.Find ("SuperMarioSmall");
+		bodyCollider = mario.GetComponent <PolygonCollider2D>();
+		animator = mario.GetComponent <Animator>();
+	}
+	
+	void startWalk(){
 		animator.SetBool ("Walk", true);
-		}
-		void stopWalk(){
+	}
+	void stopWalk(){
 		animator.SetBool ("Walk", false);
-		}
+	}
 	void faceLeft(){
 		Vector3 theScale = transform.localScale;
 		theScale.x = -1;
@@ -47,32 +52,43 @@ using System.Collections;
 		theScale.x = 1;
 		transform.localScale = theScale;
 	}
-
+	
 	void flowerPopup() {
 		animator.speed = 1;
 	}
-
+	
 	void OnCollisionEnter2D (Collision2D col)
 	{
+		jumping = false;
 		Collider2D otherCollider = col.contacts [0].otherCollider;
 		Collider2D thisCollider = col.contacts [0].collider;
 		Debug.Log ("Enter: " + otherCollider);
-		if (thisCollider == feetCollider || otherCollider == feetCollider) {
-			touchdown = true;
-			jumping = false;
+		if (otherCollider == bodyCollider) {
+			if (thisCollider.gameObject.name == "QuestionBlock" || thisCollider.gameObject.name == "Brick" || transform.position.y > touchdownPos + 0.01f) {
+				rb.velocity = Vector2.zero;
+				fallDown = true;
+			}
+		} else if (otherCollider == feetCollider) {
+			touchdownPos = transform.position.y;
+			fallDown = false;
 			animator.SetBool ("Jump", false);
-		} else if (otherCollider == headCollider) {
-			touchdown = false;
+		} else if (otherCollider == headCollider && !fallDown) {
 			if (col.gameObject.name == "QuestionBlock") {
-				foreach (Transform child in col.gameObject.transform)
-				{
-					Animator qBlockItemAnimator = child.gameObject.GetComponent <Animator>();
-					AudioSource audio = child.GetComponent <AudioSource>();
+				foreach (Transform child in col.gameObject.transform) {
+					Animator qBlockItemAnimator = child.gameObject.GetComponent <Animator> ();
+					AudioSource audio = child.GetComponent <AudioSource> ();
 					if (qBlockItemAnimator)
-						qBlockItemAnimator.SetTrigger("Start");
+						qBlockItemAnimator.SetTrigger ("Start");
 					if (audio) 
-						audio.Play();
+						audio.Play ();
+					if (col.gameObject.transform.Find ("Coin") && child.name == "PointsCanvas") {
+						Animator pointsAnimator = child.gameObject.transform.Find ("PointsText").GetComponent <Animator> ();
+						if (pointsAnimator)
+							pointsAnimator.SetTrigger ("Start");
+					} 
 				}
+			} else if (col.gameObject.name == "Brick" && !fallDown) {
+				Destroy (col.gameObject);
 			}
 		}
 	}
@@ -85,47 +101,75 @@ using System.Collections;
 		}
 	}
 
-	void FixedUpdate() {
-		if (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0) {
-			float xPos = Input.GetAxis ("Horizontal") * marioSpeed;
-			yPos = rb.velocity.y;
-			if (!jumping)
-				rb.velocity = new Vector2 (xPos, yPos);
-			else {
-				rb.velocity = new Vector2 (rb.velocity.x, jumpPower);
-				jumping = false;
-			}
+	void OnCollisionStay2D (Collision2D col) {
+		touchdown = true;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.name == "FlagPole") {
+			Debug.Log("You won! - Y pos = " + transform.position.y);
+			Debug.Break();
 		}
 	}
 
-		void Update ()
-		{
-
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.name == "GameOverCollider") {
+			GetComponent<AudioSource>().Stop();
+			other.GetComponent<AudioSource>().Play();
+		}
+	}
+	
+	void FixedUpdate() {
+		if (!fallDown && (Input.GetAxis ("Horizontal") != 0 || Input.GetAxis ("Vertical") != 0)) {
+			float xPos = Input.GetAxis ("Horizontal") * marioSpeed;
+			yPos = rb.position.y;
+			transform.position = new Vector2 (rb.position.x + xPos, yPos);
+			if (transform.position.y > maxHeight) {
+				transform.position = new Vector2 (rb.position.x, maxHeight);
+				jumping = false;
+			} else if (jump) {
+				if (rb.velocity.y >= 0) 
+					rb.velocity = new Vector2 (rb.velocity.x, jumpPower);
+				jump = false;
+			}
+		} else if (fallDown) 
+			transform.position = new Vector2 (transform.position.x, transform.position.y - 0.02f);
+	}
+	
+	void Update ()
+	{
+		
 		movingLeft = false;
 		movingRight = false;
 		stopWalk ();
-
+		
 		if (Input.GetKey ("right") && movingLeft == false) {
 			startWalk ();
 			faceRight ();
 			movingRight = true;
 		}
-
-
+		
+		
 		if (Input.GetKey ("left") && movingRight == false) {
 			startWalk ();
 			faceLeft ();
 			movingLeft = true;
 		}
-
+		
 		// Jump
-		if ((Input.GetKeyDown ("up") || Input.GetKeyDown ("space")) && !jumping && touchdown) {
+		if ((Input.GetKeyDown ("up") || Input.GetKeyDown ("space")) && !jump && touchdown) {
 			jumping = true;
 			animator.SetBool ("Jump", true);
 		}
 
+		if (Input.GetKey("up")) {
+			if (jumping && transform.position.y < (touchdownPos + jumpHeight))
+				jump = true;
+			else 
+				jumping = false;
+		}
 		
-
+		
+		
 	}
-	}
-
+}
